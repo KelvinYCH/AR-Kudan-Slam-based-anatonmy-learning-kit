972e70f765b77935c50bc8728bd597aaf987ef38
kelvin <kelvin918918918@gmail.com>
2017-04-14 22:07:00 +0800

    added marker

--- Assets/KudanAR/Scripts/Components/TrackingMethods/TrackingMethodMarkerless.cs
+++ Assets/KudanAR/Scripts/Components/TrackingMethods/TrackingMethodMarkerless.cs
@@ -14,7 +14,7 @@ namespace Kudan.AR
 		/// <summary>
 		/// The name of this tracking method.
 		/// </summary>
-		/// <value>The name.</value>
+		/// <value>The name of this tracking method is "Markerless".</value>
 		public override string Name
 		{
 			get { return "Markerless"; }
@@ -23,28 +23,25 @@ namespace Kudan.AR
 		/// <summary>
 		/// The ID of this tracking method.
 		/// </summary>
-		/// <value>The tracking method identifier.</value>
+		/// <value>The ID of this tracking method is 1.</value>
 		public override int TrackingMethodId
 		{
 			get { return 1; }
 		}
 
 		/// <summary>
-		/// The update marker event.
+		/// Event triggered each frame that ArbiTrack is running.
 		/// </summary>
 		public MarkerUpdateEvent _updateMarkerEvent;
 
 		/// <summary>
-		/// The ArbiTrack floor depth. Default value of 200.f
+		/// The ArbiTrack floor depth. Default value of 200.0f. Changing this number will affect the overall distance of the floor relative to the device.
 		/// </summary>
 		public float _floorDepth = 200.0f;
 
-        /// <summary>
-        /// Processes the current frame.
-        /// </summary>
-
-        public bool disableMarkerless = false;
-
+		/// <summary>
+		/// Processes the current frame and updates the state of ArbiTrack.
+		/// </summary>
 		public override void ProcessFrame()
 		{
             Vector3 position;
@@ -56,26 +53,25 @@ namespace Kudan.AR
             trackable.position = position;
             trackable.orientation = orientation;
 
-            if (!disableMarkerless)
-            {
-                trackable.isDetected = _kudanTracker.ArbiTrackIsTracking();
-                _updateMarkerEvent.Invoke(trackable);
-            }else
-            {
-                trackable.isDetected = false;
-            }
+            trackable.isDetected = _kudanTracker.ArbiTrackIsTracking();
+
             _updateMarkerEvent.Invoke(trackable);
 		}
 
+		/// <summary>
+		/// Starts ArbiTrack.
+		/// Sets the floor height using floorDepth, so changing floorDepth at runtime will not immediately affect tracking.
+		/// Tracking must be stopped and started again in order to see the difference.
+		/// </summary>
 		public override void StartTracking()
 		{
 			_kudanTracker.SetArbiTrackFloorHeight (_floorDepth);
 
 			base.StartTracking ();
 		}
-
-        /// <summary>
-		/// Stops tracking.
+			
+		/// <summary>
+		/// Stops ArbiTrack.
 		/// </summary>
         public override void StopTracking()
         {
---
+++ Assets/Script/MarkerMode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kudan.AR
+{
+    public class MarkerMode : MonoBehaviour
+    {
+        public KudanTracker _kudanTracker;  // The tracker to be referenced in the inspector. This is the Kudan Camera object.
+        public TrackingMethodMarker _markerTracking;    // The reference to the marker tracking method that lets the tracker know which method it is using
+                                                        // Use this for initialization
+        void Start()
+        {
+            _kudanTracker.ChangeTrackingMethod(_markerTracking);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
